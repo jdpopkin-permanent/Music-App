72295e1714a4f0dec99042cc0b188bff94524bee
App Academy Student <appacademy@Amsterdam.local>
2013-09-13 09:03:09 -0400

    Restarting with Postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicApp::Application.config.secret_token = '678074f830c6fd62ab617522c6581973bef8ce24cf0d95eeb275cdafb11091a5ae2681635436ee0b963f2319b13a7fbf8fb5353a0ea9d8c17336f4bde836caf2'
