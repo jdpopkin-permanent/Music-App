fbb5113abf447eda25b367bce73147c4d0b0d9a6
Jackson Popkin <jdpopkin@gmail.com>
2013-09-13 17:49:04 -0400

    Adds ugly_lyrics helper.
    Lyrics are now displayed on track pages, as formatted by
    ugly_lyrics.

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,14 @@
 module TracksHelper
+  def ugly_lyrics(lyrics)
+    str = "<pre>"
+
+    lyrics.split("\n").each do |line|
+      str << "&#9835 "
+      str << h(line)
+      str << "\n"
+    end
+
+    str << "</pre>"
+    str.html_safe
+  end
 end
